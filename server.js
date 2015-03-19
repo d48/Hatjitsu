bf953a7e8879f8550baf212f5d35848be5a2d0f3
Ryan Regalado <Ryan.Regalado@blackline.com>
2015-03-19 14:27:26 -0700

    update port listener

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ io.configure('development', function(){
 });
 
 var port = process.env.app_port || 5000; // Use the port that Heroku provides or default to 5000
-app.listen(port, function() {
+app.listen(process.env.PORT || port, function() {
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
